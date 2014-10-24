96abbc0af77da6356567a2b818c6a8e8283fba29
unknown <b.poulard@.domaine.local>
2014-10-24 16:46:53 +0200

    Ajout de "l'intelligence" php / json et mise en place du formulaire

--- ident.php
+++ ident.php
@@ -1,5 +1,20 @@
 <?php
-$listIdent = fopen("ident.json", "r+");
-var_dump($listIdent);
+
+$nom = htmlspecialchars($_POST['chpNom']);
+$prenom = htmlspecialchars($_POST['chpPrenom']);
+$email = htmlspecialchars($_POST['chpEmail']);
+
+$listIdent = file_get_contents("ident.json");
+$listArray = json_decode($listIdent,true);
+
+echo "<pre>" . var_export($listArray) . "</pre>";
+echo "<h1>" . count($listArray['liste']) . "</h1>";
+
+echo "<ul><li>" . $nom . "<li>" . $prenom . "<li>" . $email . "</ul>";
+
+array_push ($listArray['liste'], array('nom'=>$nom,'prenom'=>$prenom,'email'=>$email));
+
+$newListArray = json_encode($listArray);
+file_put_contents("ident.json", $newListArray);
 
 ?>
